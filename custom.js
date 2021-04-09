4a1e4a445acbd28c1b567e49edd7a3b91be65ba8
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2021-04-09 21:28:54 +0200

    fixes

--- custom.js
+++ custom.js
@@ -5,10 +5,14 @@ let currentDate = new Date();
 let month = currentDate.getUTCMonth() + 1;
 let day = currentDate.getUTCDay();
 
-if ( month >= 4 && month <= 8 ) {
-    promotionText.innerHTML = "Voor u is nu het heerlijke lente-zomer arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+if ( month >= 3 && month <= 5 ) {
+    promotionText.innerHTML = "Voor u is nu het heerlijke lente arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "zomer-arrangement.html";
-} else if (month >= 9 || month <= 3) {
+}
+else if(month >= 6 && month <= 9) {
+    //
+}
+else if (month >= 10 || month <= 2) {
     promotionText.innerHTML = "Voor u is nu het heerlijke herfst-winter arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "herfst-winter-massage.html";
 }
