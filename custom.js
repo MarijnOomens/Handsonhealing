de9732efc21417ab320a8e92a4fb716a14e9b653
MarijnOomensAvans <31892254+MarijnOomens@users.noreply.github.com>
2021-04-18 12:27:44 +0200

    lente-voorjaar en mindfullness schilderen

--- custom.js
+++ custom.js
@@ -6,11 +6,12 @@ let month = currentDate.getUTCMonth() + 1;
 let day = currentDate.getUTCDay();
 
 if ( month >= 3 && month <= 5 ) {
-    promotionText.innerHTML = "Voor u is nu het heerlijke lente arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
-    seizoensAanbiedingButton.href = "zomer-arrangement.html";
+    promotionText.innerHTML = "Voor u is nu het heerlijke lente voorjaar arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+    seizoensAanbiedingButton.href = "lente-voorjaar-arrangement.html";
 }
 else if(month >= 6 && month <= 9) {
-    //
+    promotionText.innerHTML = "Voor u is nu het heerlijke zomer arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+    seizoensAanbiedingButton.href = "zomer-arrangement.html";
 }
 else if (month >= 10 || month <= 2) {
     promotionText.innerHTML = "Voor u is nu het heerlijke herfst-winter arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
