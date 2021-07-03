c6433dfa120fadc3a21c3e36875aa670fbf4224b
MarijnOomensAvans <31892254+MarijnOomens@users.noreply.github.com>
2021-07-03 16:12:25 +0200

    Koringen update

--- custom.js
+++ custom.js
@@ -6,14 +6,14 @@ let month = currentDate.getUTCMonth() + 1;
 let day = currentDate.getUTCDay();
 
 if ( month >= 3 && month <= 5 ) {
-    promotionText.innerHTML = "Voor u is nu het heerlijke lente voorjaar arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+    promotionText.innerHTML = "Voor u is nu het heerlijke lente voorjaar arrangement in de aanbieding. Normaal €72,00, nu €67,50. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "lente-voorjaar-arrangement.html";
 }
 else if(month >= 6 && month <= 9) {
-    promotionText.innerHTML = "Voor u is nu het heerlijke zomer arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+    promotionText.innerHTML = "Voor u is nu het heerlijke zomer arrangement in de aanbieding. Normaal €72,00, nu €67,50. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "zomer-arrangement.html";
 }
 else if (month >= 10 || month <= 2) {
-    promotionText.innerHTML = "Voor u is nu het heerlijke herfst-winter arrangement in de aanbieding. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+    promotionText.innerHTML = "Voor u is nu het heerlijke herfst-winter arrangement in de aanbieding. Normaal €72,00, nu €67,50. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "herfst-winter-massage.html";
 }
