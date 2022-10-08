73f1a54dd619feba3a06cb3f76c6e20046949429
Marijn <31892254+MarijnOomens@users.noreply.github.com>
2022-10-08 16:21:06 +0200

    update tijden en prijs najaar arrangement

--- custom.js
+++ custom.js
@@ -5,15 +5,15 @@ let currentDate = new Date();
 let month = currentDate.getUTCMonth() + 1;
 let day = currentDate.getUTCDay();
 
-if ( month >= 3 && month <= 5 ) {
+if (month >= 3 && month <= 5) {
     promotionText.innerHTML = "Voor u is nu het heerlijke lente voorjaar arrangement in de aanbieding. Normaal €72,00, nu €67,50. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "lente-voorjaar-arrangement.html";
 }
-else if(month >= 6 && month <= 9) {
+else if (month >= 6 && month <= 9) {
     promotionText.innerHTML = "Voor u is nu het heerlijke zomer arrangement in de aanbieding. Normaal €72,00, nu €67,50. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "zomer-arrangement.html";
 }
 else if (month >= 10 || month <= 2) {
-    promotionText.innerHTML = "Voor u is nu het heerlijke najaar winter arrangement in de aanbieding. Normaal €72,00, nu €67,50. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
+    promotionText.innerHTML = "Voor u is nu het heerlijke najaar winter arrangement in de aanbieding. Normaal €72,00, nu €68,00. Klik voor een uitgebreide beschrijving van deze behandeling hieronder.";
     seizoensAanbiedingButton.href = "najaar-winter-arrangement.html";
 }
